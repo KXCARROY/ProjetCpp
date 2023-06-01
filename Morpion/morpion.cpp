50395248c6bd61a6233f3e730579b100a787a32a
KXCARROY <kcarroyarvy@gmail.com>
2023-06-01 21:19:29 +0200

    Init bouton et connexion signaux

--- Morpion/morpion.cpp
+++ Morpion/morpion.cpp
@@ -12,6 +12,22 @@ Morpion::Morpion(QWidget *parent) : QWidget(parent), ui(new Ui::Morpion)
     connect(&socket, &QTcpSocket::readyRead, this, &Morpion::readyRead);
     socket.connectToHost("localhost", 9999);  // Connexion au serveur
 
+    //Initialilisation des boutons et connexion à la méthode onButtonClicked
+    buttons = QVector<QVector<QPushButton*>>(7,QVector<QPushButton*>(6));
+    QSignalMapper* mapper = new QSignalMapper(this);
+    for (int i = 0; i < 7; ++i) {
+        for (int j = 0; j < 6; ++j) {
+            buttons[i][j] = new QPushButton(this);
+
+            // Connexion du signal clicked du bouton au slot map du mapper
+            connect(buttons[i][j], SIGNAL(clicked()), mapper, SLOT(map()));
+
+            // Association du bouton avec un index dans le mapper et convertir des coordonnées bidimensionnelles en index unidimensionnel
+            mapper->setMapping(buttons[i][j], i * 7 + j);
+        }
+    }
+    // Connexion du signal du mapper au slot onButtonClicked
+    connect(mapper, SIGNAL(mapped(int)), this, SLOT(onButtonClicked(int)));
 }
 
 
